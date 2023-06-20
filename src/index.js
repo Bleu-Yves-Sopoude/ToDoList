19b611089b3e61f36f2d3962f6905dd2b918aaec
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-06-20 10:34:27 +0000

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -23,12 +23,11 @@ function printHTML(description, status, index) {
     'beforeend',
     `<div class="to-do-list__box" index="${index}" completed="${status}">
 <div class="d-row box__icon-text-wraper">
-        <i class="bi bi-square to-do-box__check-box-icon"></i>
-        <p class="box__text--incomplete">${description}</p>
-      </div>
-      <i class="bi bi-three-dots-vertical to-do-box__three-dots-icon"></i>
-    </div>
-    `,
+<i class="bi bi-square to-do-box__check-box-icon"></i>
+<p class="box__text--incomplete">${description}</p>
+</div>
+<i class="bi bi-three-dots-vertical to-do-box__three-dots-icon"></i>
+</div>`,
   );
 }
 
