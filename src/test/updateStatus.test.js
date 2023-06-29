2b1f02e983539bfb8eb7370ae0331a5b41b8657f
Peter-Kibet <peter.kibet@hotmail.com>
2023-06-29 19:37:37 +0300

    Fix linter errors

--- src/test/updateStatus.test.js
+++ src/test/updateStatus.test.js
@@ -1,18 +1,19 @@
-
 // Path: src/test/updateStatus.test.js
-import { updateTaskStatus } from './updateStatus.js';
+import { updateTaskStatus } from "./updateStatus.js";
 
-describe('updateTaskStatus', () => {
-  test('updates task status and calls saveTasks', () => {
+describe("updateTaskStatus", () => {
+  test("updates task status and calls saveTasks", () => {
+    // This function updates the completed status of a task, and calls the
+    // saveTasks function.
     const index = 1;
     const completed = true;
 
     const mockSaveTasks = jest.fn();
 
     const tasks = [
-      { description: 'Task 1' },
-      { description: 'Task 2' },
-      { description: 'Task 3' }
+      { description: "Task 1" },
+      { description: "Task 2" },
+      { description: "Task 3" },
     ];
 
     updateTaskStatus(index, completed, tasks, mockSaveTasks);
@@ -22,3 +23,25 @@ describe('updateTaskStatus', () => {
   });
 });
 
+// Path: src/test/updateStatus.test.js
+import { updateTaskStatus } from "./updateStatus.js";
+
+describe("updateTaskStatus", () => {
+  test("updates task status and calls saveTasks", () => {
+    const index = 1;
+    const completed = true;
+
+    const mockSaveTasks = jest.fn();
+
+    const tasks = [
+      { description: "Task 1" },
+      { description: "Task 2" },
+      { description: "Task 3" },
+    ];
+
+    updateTaskStatus(index, completed, tasks, mockSaveTasks);
+
+    expect(tasks[index].completed).toBe(completed);
+    expect(mockSaveTasks).toHaveBeenCalled();
+  });
+});
