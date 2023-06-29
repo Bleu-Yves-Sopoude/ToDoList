5f09be70ea4e9df5375ae39a7edf0f90ef46e1d3
Peter-Kibet <peter.kibet@hotmail.com>
2023-06-29 19:48:32 +0300

    Check functionality and fix linter errors

--- src/test/updateStatus.test.js
+++ src/test/updateStatus.test.js
@@ -3,31 +3,6 @@ import { updateTaskStatus } from "./updateStatus.js";
 
 describe("updateTaskStatus", () => {
   test("updates task status and calls saveTasks", () => {
-    // This function updates the completed status of a task, and calls the
-    // saveTasks function.
-    const index = 1;
-    const completed = true;
-
-    const mockSaveTasks = jest.fn();
-
-    const tasks = [
-      { description: "Task 1" },
-      { description: "Task 2" },
-      { description: "Task 3" },
-    ];
-
-    updateTaskStatus(index, completed, tasks, mockSaveTasks);
-
-    expect(tasks[index].completed).toBe(completed);
-    expect(mockSaveTasks).toHaveBeenCalled();
-  });
-});
-
-// Path: src/test/updateStatus.test.js
-import { updateTaskStatus } from "./updateStatus.js";
-
-describe("updateTaskStatus", () => {
-  test("updates task status and calls saveTasks", () => {
     const index = 1;
     const completed = true;
 
